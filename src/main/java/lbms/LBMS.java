5e70f96084f9f5a062718f92add54a5e64cebd8b
APalumbo97 <ajpalumbo97@gmail.com>
2017-02-23 13:04:26 -0500

    Added main class.

---
+++ src/main/java/lbms/LBMS.java
@@ -0,0 +1,17 @@
+package lbms;
+
+/**
+ * @author Team B
+ * Created: 2-23-2017
+ * Description: Main class to run the Library Book Management System.
+ */
+public class LBMS {
+
+    /**
+     * Main method.
+     * @param args: the program arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
