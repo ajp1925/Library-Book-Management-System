4e48a3efc9e86a8bda93943a980afbace3c53ff1
Nicholas <nickcfeldman@gmail.com>
2017-02-28 13:36:02 -0500

    Create new instances of states instead of entering the old one

--- src/main/java/lbms/state/StateManager.java
+++ src/main/java/lbms/state/StateManager.java
@@ -2,19 +2,31 @@ package lbms.state;
 
 public class StateManager {
 
-    public static State STATE_DEFAULT = new StateDefault();
-    public static State STATE_USERS = new StateUsers();
-    public static State STATE_BOOKS = new StateBooks();
-    public static State STATE_USERS_REGISTER = new StateUsersRegister();
+    public static Class STATE_DEFAULT = StateDefault.class;
+    public static Class STATE_USERS = StateUsers.class;
+    public static Class STATE_BOOKS = StateBooks.class;
+    public static Class STATE_USERS_REGISTER = StateUsersRegister.class;
 
     private static State currentState;
 
-    public static void setState(State state) {
-        currentState = state;
-        state.onEnter();
-        state.display();
+    /**
+     * Creates a new instance of the given state class and enters.
+     * @param clazz The state to switch to
+     */
+    public static void setState(Class<? extends State> clazz) {
+        try {
+            currentState = clazz.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        currentState.onEnter();
+        currentState.display();
     }
 
+    /**
+     * @return The current state
+     */
     public static State getState() {
         return currentState;
     }
