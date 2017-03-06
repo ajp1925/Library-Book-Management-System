4952eb4b474c487c23b148f5089a0ed967b95d66
Nicholas <nickcfeldman@gmail.com>
2017-03-06 16:36:08 -0500

    Complete Return

--- src/main/java/lbms/command/Return.java
+++ src/main/java/lbms/command/Return.java
@@ -1,13 +1,50 @@
 package lbms.command;
 
+import lbms.API;
+import lbms.models.Transaction;
+import lbms.models.Visitor;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 /**
- * Created by anthony on 3/5/17.
+ * Returns a book borrowed by a library visitor.
+ * @author Team B
  */
 public class Return implements Command {
 
-    @Override
-    public String execute() {
-        // TODO Nick
+    long visitorID;
+    List<Integer> ids = new ArrayList<>();
+
+    public Return(String request) {
+        request = request.replaceAll(";$", "");
+        String[] split = request.split(",", 1);
+        visitorID = Long.parseLong(split[0]);
+
+        String[] idStrings = split[1].split(",");
+
+        ids = Arrays.stream(split[1].split(",")).map(Integer::parseInt).collect(Collectors.toList());
     }
 
+    @Override
+    public String execute() {
+        if (!API.visitorIsRegisteredID(visitorID)) return "invalid-visitor-id;";
+        Visitor visitor = API.getVisitorByID(visitorID);
+
+        if (visitor.getNumCheckedOut() < ids.size()) return "invalid-book-id," +
+                ids.toString().substring(1, ids.size()-1).replaceAll("\\s", "") + ";";
+
+
+        int i = 1;
+        for (Map.Entry<Long, Transaction> longTransactionEntry : visitor.getCheckedOutBooks().entrySet()) {
+            if (ids.contains(i))
+                if (API.getSystemDate().isAfter(longTransactionEntry.getValue().getDueDate()))
+                    return "overdue," + longTransactionEntry.getValue().getFine() + "," + i;
+        }
+
+        return "success;";
+    }
 }
