51d97f0231971c54ff2bacd069ab4538180c77c8
exw4141 <exw4141@rit.edu>
2017-03-07 00:11:54 -0500

    Added more implementation to execute of StoreSearch command

--- src/main/java/lbms/command/StoreSearch.java
+++ src/main/java/lbms/command/StoreSearch.java
@@ -1,6 +1,11 @@
 package lbms.command;
 
+import lbms.API;
+import lbms.models.Book;
+import lbms.search.SearchByTitle;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by anthony on 3/5/17.
@@ -35,7 +40,29 @@ public class StoreSearch implements Command {
         if (sortOrder != "title" && sortOrder != "publish-date") {
             return "invalid-sort-order";
         }
-        return "";
+        int booksFound = 0;
+        SearchByTitle search = new SearchByTitle(title);
+        List<Book> books = API.findBooks(search);
+
+        if (books.size() == 0) {
+            return Integer.toString(booksFound) + ";";
+        }
+        else {
+            int id = 1;
+            String response = Integer.toString(books.size()) + "\n";
+            for (Book book : books) {
+                response = response + Integer.toString(id) + "," + book.getIsbn() + "," + book.getTitle()
+                + ",{";
+                for (String author : book.getAuthors()) {
+                    response = response + author + ",";
+                }
+                response = response.replaceAll(",$", "},");
+                response = response + book.getPublishDate() + "\n";
+                response = response.replaceAll("\\n$", ";");
+                id += 1;
+            }
+            return response;
+        }
     }
 
 }
