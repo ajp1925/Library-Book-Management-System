351256e188c5ab41a133942ef0be4c4f6a9906ff
Nicholas <nickcfeldman@gmail.com>
2017-03-08 14:49:25 -0500

    Deprecate API Class

--- src/main/java/lbms/API.java
+++ src/main/java/lbms/API.java
@@ -14,6 +14,7 @@ import java.util.List;
  * Facade class for interacting with the system.
  * @author Team B
  */
+@Deprecated
 public class API {
 
     public static DecimalFormat df = new DecimalFormat("#.00");
